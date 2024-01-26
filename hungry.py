464c61668997cbf386bdf823ac95354cdfb3bec7
Aakanksha Khedkar <akanshakhedkar@gmail.com>
2024-01-26 22:14:36 +0530

    water

--- hungry.py
+++ hungry.py
@@ -5,4 +5,6 @@ if hungry=="yes":
     print("eat samosa")
     print("eat pizza")
 else:
-    print("do your homework")
+    thirsty=input("are you thirsty")
+    if thirsty=="yes":
+        print("drink water")
